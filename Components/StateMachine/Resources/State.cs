893da64c8400771803767aa035bf33ddb4fd3c80
Gamemaster-git <human.choupani@gmail.com>
2025-07-17 01:12:44 +0330

    Added State Machine.
    Added a state machine and an abstract state class.

---
+++ Components/StateMachine/Resources/State.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+[GlobalClass]
+public partial class State : Node
+{
+    [Signal]
+    public delegate void TransitionedEventHandler(State state, StringName newStateName);
+
+
+    public CharacterBody2D parent;
+
+
+    public virtual void Enter() { }
+
+    public virtual void Exit() { }
+
+    // Triggered by respective built in godot functions within parent state machine.
+    public virtual void InputUpdate(InputEvent input) { }
+
+    public virtual void Update(double delta) { }
+
+    public virtual void PhysicsUpdate(double delta) { }
+}
---
+++ Components/StateMachine/Resources/StateMachine.cs
@@ -0,0 +1,88 @@
+using Godot;
+
+using System.Collections.Generic;
+
+public partial class StateMachine : Node
+{
+    [ExportGroup("State")]
+    [Export]
+    private State initialState;
+    [ExportGroup("Refrences")]
+    [Export]
+    private CharacterBody2D parent;
+
+    public State currentState { get; set; }
+
+    public Dictionary<StringName, State> states { get; set; } = new Dictionary<StringName, State>();
+
+
+    public override void _Ready()
+    {
+        foreach (State child in GetChildren())
+        {
+
+            states.Add(child.Name, child);
+            child.Transitioned += (State state, StringName newStateName) =>
+            {
+                OnStateTransition(state, newStateName);
+            };
+        }
+
+        if (initialState is State)
+        {
+            initialState.parent = parent;
+            initialState.Enter();
+            currentState = initialState;
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        if (currentState is State)
+        {
+            currentState.Update(delta);
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (currentState is State)
+        {
+            currentState.PhysicsUpdate(delta);
+        }
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (currentState is State)
+        {
+            currentState.InputUpdate(@event);
+        }
+    }
+
+
+    private void OnStateTransition(State state, StringName newStateName)
+    {
+
+        if (state != currentState)
+        {
+            return;
+        }
+
+        State newState = states[newStateName];
+        if (newState is null)
+        {
+            return;
+        }
+
+        if (currentState is State)
+        {
+            currentState.Exit();
+        }
+
+        newState.parent = parent;
+        newState.Enter();
+
+        currentState = newState;
+    }
+}
